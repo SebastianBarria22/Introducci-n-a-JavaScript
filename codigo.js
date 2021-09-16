372f1ddc1b90f8fa206ae01cd5199258613ea1b2
SebastianBarria22 <71775331+SebastianBarria22@users.noreply.github.com>
2021-09-15 21:11:15 -0300

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -1,20 +1,11 @@
-// Obtenemos el button y lo almacenamos en una variable llamada "btn"
-var btn = document.getElementById("btn");
-/* Obtenemos el div que muestra el resultado y lo
-almacenamos en una variable llamada "resultado" */
-var resultado = document.getElementById("random")
-/* Obtenemos los dos input y los almacenamos en
-variables "inputUno" y "inputDos" */
-var inputUno = document.getElementById("input-uno");
-var inputDos = document.getElementById("input-dos");
-// Añadimos el evento click a la variable "btn"
-btn.addEventListener("click",function(){
-/* Obtenemos el valor de cada input accediendo a
-su atributo "value" */
-var n1 = inputUno.value;
-var n2 = inputDos.value;
-resultado.innerHTML = aleatorio(n1,n2);
+// Obtenemos el DIV y lo almacenamos en una variable llamada "caja"
+var caja = document.getElementById("caja");
+// Obtenemos el span y lo almacenamos en una variable llamada "texto"
+var texto = document.getElementById("texto");
+// Añadimos el evento click a la variable "caja"
+caja.addEventListener("click",function(){
+// Cambiamos el texto del span que almacenamos en la variable "texto"
+texto.innerHTML = "He cambiado el texto!";
+// Cambiamos el color del DIV que almacenamos en la variable "caja"
+caja.style.background = "orange";
 });
-function aleatorio(n1,n2){
-    return Math.round(Math.random()*(n2-n1)+parseInt(n1))
-}
